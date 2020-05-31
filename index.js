3ba3a2ef038b4df4d6cc5f168487a73e63d7aafe
Leandro <llimas@outlook.com>
2020-05-31 15:28:46 -0300

    Primeira versão produção

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+const app = express()
+app.use(express.static('public'))
+const port = 3000
+nunjucks.configure('views', {
+    autoescape: true,
+    express: app
+});
+app.set('views', './views')
+app.get('/', (req, res) => res.render('home.html'))
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))

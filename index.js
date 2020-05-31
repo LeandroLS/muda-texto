5d33dc5ab4b5d215a42b1f81d0ecb45ef33d6463
Leandro <llimas@outlook.com>
2020-05-31 17:47:03 -0300

    url redirect

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const nunjucks = require('nunjucks')
 const app = express()
+require('dotenv').config()
 app.use(express.static('public'))
 const port = 3000
 nunjucks.configure('views', {
     autoescape: true,
     express: app
 });
+function wwwRedirect(req, res, next) {
+    if(process.env.AMBIENTE == 'production'){
+        let host = req.headers.host;
+        if (host.slice(0, 4) === 'www.' && (req.protocol === 'http' || req.protocol === 'https')) {
+            let newHost = host.slice(4);
+            return res.redirect(301, 'https://' + newHost + req.originalUrl);
+        } else if (req.protocol === 'http') {
+            return res.redirect(301, 'https://' + req.headers.host + req.originalUrl);
+        }
+    }
+    next();
+};
+app.use(wwwRedirect);
 app.set('views', './views')
 app.get('/', (req, res) => res.render('home.html'))
-app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Convertexto listening at http://localhost:${port}`))

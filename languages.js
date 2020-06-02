4005e801086887eec20802c91e7b58ae16cd3fb1
Leandro <llimas@outlook.com>
2020-06-02 19:31:39 -0300

    Espanhol

--- languages.js
+++ languages.js
@@ -1,20 +1,16 @@
 var languages = {
-    
-        
-            pt: { 
-                title: 'Converter Texto - Ferramentas',
-                description: 'Conversão de texto entre minúsculas e maiúsculas, texto reverso, cabeça pra baixo, texto para aúdio, contador de letras e muito mais.'
-            },
-        
-   
-
-        
-            en: {
-                title: 'Convert Text - Tool',
-                description: 'Conversion of text between lowercase and uppercase, reverse text, upside down, text to audio, letter counter and more.'
-            }
-        
-    
+        pt: { 
+            title: 'Converter Texto - Ferramentas',
+            description: 'Conversão de texto entre minúsculas e maiúsculas, texto reverso, cabeça pra baixo, texto para aúdio, contador de letras e muito mais.'
+        },
+        en: {
+            title: 'Convert Text - Tool',
+            description: 'Conversion of text between lowercase and uppercase, reverse text, upside down, text to audio, letter counter and more.'
+        },
+        es: {
+            title: 'Convertir texto - Herramientas',
+            description: 'Conversión de texto entre minúsculas y mayúsculas, texto inverso, al revés, texto a audio, contador de letras y más.'
+        }
     }
 
 module.exports = languages

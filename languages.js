7ab76c737913b46e6dda9f32a68be02d1ab8b739
Leandro <llimas@outlook.com>
2020-06-13 17:09:30 -0300

    Adicionando italiano

--- languages.js
+++ languages.js
@@ -1,15 +1,19 @@
 var languages = {
         pt: { 
-            title: 'Converter Texto - Ferramentas',
+            title: 'Converter Texto - Conversão de textos',
             description: 'Conversão de texto entre minúsculas e maiúsculas, texto reverso, cabeça pra baixo, texto para aúdio, contador de letras e muito mais.'
         },
         en: {
-            title: 'Convert Text - Tool',
+            title: 'Convert Text - Conversion of texts',
             description: 'Conversion of text between lowercase and uppercase, reverse text, upside down, text to audio, letter counter and more.'
         },
         es: {
-            title: 'Convertir texto - Herramientas',
+            title: 'Convertir texto - Conversión de textos',
             description: 'Conversión de texto entre minúsculas y mayúsculas, texto inverso, al revés, texto a audio, contador de letras y más.'
+        },
+        it: {
+            title: 'Converti testo - Conversione testo',
+            description: 'Conversione di testo tra lettere minuscole e maiuscole, testo inverso, sottosopra, testo audio, contatore di lettere e molto altro.'
         }
     }
 

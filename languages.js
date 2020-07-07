7a7079b3686de0475963e903c9e6242807c12fd5
Leandro <llimas@outlook.com>
2020-07-06 21:35:01 -0300

    mudando description

--- languages.js
+++ languages.js
@@ -1,23 +1,23 @@
 var languages = {
         pt: { 
             title: 'Converter Texto - Conversão de textos',
-            description: 'Conversão de texto entre minúsculas e maiúsculas, texto reverso, cabeça pra baixo, texto para aúdio, contador de letras e muito mais.'
+            description: 'Conversão de texto entre minúsculas e maiúsculas, texto reverso, cabeça pra baixo, texto para aúdio, contador de caracteres, palavras e linhas.'
         },
         en: {
             title: 'Convert Text - Conversion of texts',
-            description: 'Conversion of text between lowercase and uppercase, reverse text, upside down, text to audio, letter counter and more.'
+            description: 'Conversion of text between lowercase and uppercase, reverse text, upside down, text to audio, character, word and line counter'
         },
         es: {
             title: 'Convertir texto - Conversión de textos',
-            description: 'Conversión de texto entre minúsculas y mayúsculas, texto inverso, al revés, texto a audio, contador de letras y más.'
+            description: 'Conversión de texto entre minúsculas y mayúsculas, texto inverso, al revés, texto a audio, contador de caracteres, palabras y líneas'
         },
         it: {
             title: 'Converti testo - Conversione testo',
-            description: 'Conversione di testo tra lettere minuscole e maiuscole, testo inverso, sottosopra, testo audio, contatore di lettere e molto altro.'
+            description: 'Conversione di testo tra lettere minuscole e maiuscole, testo inverso, sottosopra, testo audio, contatore di caratteri, parole e righe.'
         },
         fr: {
             title: 'Convertir du texte - Conversion de texte',
-            description: 'Conversion de texte entre minuscules et majuscules, texte inversé, à l\'envers, texte audio, compteur de lettres et bien plus encore.'
+            description: 'Conversion de texte entre minuscules et majuscules, texte inversé, à l\'envers, texte audio, compteur de caractères, de mots et de lignes.'
         }
     }
 
